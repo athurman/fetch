f4aa8c5654468f99e42a2184d3092a8164de3022
amanda-thrive <amanda@thrivemg.com>
2014-02-01 14:43:06 -0600

    refactor JsonParser class

--- json_parser.rb
+++ json_parser.rb
@@ -16,27 +16,18 @@ class JsonParser
     breeds_hash = JSON.parse(breeds)
     breeds_hash["breeds"].each do |breed_obj|
       name = breed_obj["breed_name"]
-      puts name
-      size = breed_obj["size"]
-      lifespan = breed_obj["lifespan"]
-      weight = breed_obj["weight"]
-      height = breed_obj["height"]
       group = Group.find(breed_obj["group"])
-      exercise = breed_obj["exercise"]
-      grooming = breed_obj["grooming"]
-      family_friendly = breed_obj["family_friendly"]
       role = Role.find(breed_obj["role"])
-      temperament = breed_obj["temperament"]
       name.each_char do |char|
         if char == "'"
           name.delete!(char)
         end
       end
-
-      breed = Breed.create(name: name, size: size, lifespan: lifespan,
-                           weight: weight, height: height, group_id: group.id,
-                           exercise: exercise, grooming: grooming, family_friendly: family_friendly,
-                           role_id: role.id, temperament: temperament)
+      breed = Breed.create(name: name, size: breed_obj["size"], lifespan: breed_obj["lifespan"],
+                           weight: breed_obj["weight"], height: breed_obj["height"], group_id: group.id,
+                           exercise: breed_obj["exercise"], grooming: breed_obj["grooming"],
+                           family_friendly: breed_obj["family_friendly"], role_id: role.id,
+                           temperament: breed_obj["temperament"])
     end
   end
 
@@ -44,11 +35,6 @@ class JsonParser
     response = HTTParty.get(BASEURL + 'pet.find?key=c75009674079754cf0b3a9551492bbb3&animal=dog&location=37218&count=5&format=json&token=95161f1daa0b100070ceb16cfe757aeb&sig=d6b2a331f1256cc4bb1e28bcb98d7b14')
     shelter_dogs_ary = JSON.parse(response.body)
     shelter_dogs_ary["petfinder"]["pets"]["pet"].each do |dog_hash|
-
-      name =  dog_hash["name"]["$t"]
-      status =  dog_hash["status"]["$t"]
-      age =  dog_hash["age"]["$t"]
-      weight = dog_hash["size"]["$t"]
       shelter = dog_hash["shelterId"]["$t"]
       if dog_hash["breeds"]["breed"].is_a?(Array)
         breed = dog_hash["breeds"]["breed"][0]["$t"]
@@ -56,9 +42,9 @@ class JsonParser
         breed = dog_hash["breeds"]["breed"]["$t"]
       end
       shelterdog_breed = Breed.find_by_name(breed)
-      shelterdog = ShelterDog.create(name: name, breed_id: shelterdog_breed.id,
-                                     shelter: shelter, age: age,
-                                     weight: weight, status: status)
+      shelterdog = ShelterDog.create(name: dog_hash["name"]["$t"], breed_id: shelterdog_breed.id,
+                                     shelter: shelter, age: dog_hash["age"]["$t"],
+                                     weight: dog_hash["size"]["$t"], status: dog_hash["status"]["$t"])
     end
   end
 end
