03fa6a24839d5c1cda2db054f0b9b669b45492e3
amanda-thrive <amanda@thrivemg.com>
2014-02-05 13:17:15 -0600

    dog image to logo

--- test/test_listing_all_shelterdogs.rb
+++ test/test_listing_all_shelterdogs.rb
@@ -15,18 +15,17 @@ class TestListingShelterDogs < FetchTest
     command = "./fetchdog list"
     expected = <<EOS.chomp
 clear
+       ,
+       |`-.__     ad88                                 88
+       / ' _/    d8\"                 ,d                88
+      ****`      88                  88                88
+     /    }    MM88MMM  ,adPPYba,  MM88MMM  ,adPPYba,  88,dPPYba,
+    /  \\ /       88    a8P_____88    88    a8\"     \"\"  88P'    \"8a
+\\ /`   \\\\\\       88    8PP\"\"\"\"\"\"\"    88    8b          88       88
+ `\\    /_\\\\      88    \"8b,   ,aa    88,   \"8a,   ,aa  88       88
+  `~~~~~``~`     88     `\"Ybbd8\"'    \"Y888  `\"Ybbd8\"'  88       88
 
-
-       ad88                                 88
-      d8\"                 ,d                88
-      88                  88                88
-    MM88MMM  ,adPPYba,  MM88MMM  ,adPPYba,  88,dPPYba,
-      88    a8P_____88    88    a8\"     \"\"  88P'    \"8a
-      88    8PP\"\"\"\"\"\"\"    88    8b          88       88
-      88    \"8b,   ,aa    88,   \"8a,   ,aa  88       88
-      88     `\"Ybbd8\"'    \"Y888  `\"Ybbd8\"'  88       88
-
-                    -- GO FOR IT --
+                                 -- GO FOR IT --
 
 All Shelter Dogs:
 #{fido.id}. Fido:
