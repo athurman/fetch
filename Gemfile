22e662c7dd0bf2efbfa617d29d0e018a8f0c9e6f
amanda-thrive <amanda@thrivemg.com>
2014-02-05 11:21:28 -0600

    include activerecord gem and refactor require models

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source "https://rubygems.org"
 ruby "2.0.0"
 #ruby-gemset=fetch
 
+gem 'activerecord'
 gem 'sqlite3'
 gem 'httparty'
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,13 @@
 #!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler/setup'
+require 'active_record'
+
+project_root = File.dirname(File.absolute_path(__FILE__))
+Dir.glob(project_root + "/../models/*.rb").each{|f| require f}
+
 require_relative 'database'
-require_relative '../models/shelterdog'
 
 
 class Environment
