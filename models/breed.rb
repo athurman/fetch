f3f54a63005698c2cad55e4b8a1d877129cf3c49
amanda-thrive <amanda@thrivemg.com>
2014-02-07 13:22:19 -0600

    Breed model class now refactored to include activerecord

--- models/breed.rb
+++ models/breed.rb
@@ -1,147 +1,27 @@
 class Breed < ActiveRecord::Base
-  # attr_accessor :name,
-  #               :size,
-  #               :lifespan,
-  #               :weight,
-  #               :height,
-  #               :group,
-  #               :exercise,
-  #               :grooming,
-  #               :family_friendly,
-  #               :role_id,
-  #               :temperament
-  # attr_reader :id
   belongs_to :role
   belongs_to :group
-
-  # def initialize attributes = {}
-  #   @name = attributes[:name]
-  #   @size = attributes[:size]
-  #   @lifespan = attributes[:lifespan]
-  #   @weight = attributes[:weight]
-  #   @height = attributes[:height]
-  #   @group = Group.find_by_id(attributes[:group_id])
-  #   @exercise = attributes[:exercise]
-  #   @grooming = attributes[:grooming]
-  #   @family_friendly = attributes[:family_friendly]
-  #   @role_id = attributes[:role_id]
-  #   @temperament = attributes[:temperament]
-  # end
 
   def to_s
     "#{id}. #{name}:\nSize: #{size}\nLifespan: #{lifespan}\nAverage Weight: #{weight}\nAverge Height: #{height}\nGroup: #{group.name}\nExercise: #{exercise}\nGrooming? #{grooming}\nFamily Friendly? #{family_friendly}\nTemperament: #{temperament}"
   end
 
-  # def save
-  #   database = Environment.database_connection
-  #   database.execute("insert into breeds(name, size, lifespan, weight, height, group_id, exercise, grooming, family_friendly, role_id, temperament) values('#{name}', '#{size}', '#{lifespan}', '#{weight}', '#{height}', '#{group.id}', '#{exercise}', '#{grooming}', '#{family_friendly}', '#{role_id}', '#{temperament}')")
-  #   @id = database.last_insert_row_id
-  # end
-
-  # def self.create(attributes = {})
-  #   breed = Breed.new(attributes)
-  #   breed.save
-  #   breed
-  # end
-
-  def self.find_by_name breed_name
-    database = Environment.database_connection
-    database.results_as_hash = true
-    results = database.execute("select * from breeds where name = '#{breed_name}'")
-    database.results_as_hash = false
-    if results.empty?
-      breed = "Unable to find breed"
-    else
-      row_hash = results[0]
-      # group = Group.find_by_id(row_hash["group_id"])
-      breed = Breed.new(name: row_hash["name"], size: row_hash["size"], lifespan: row_hash["lifespan"],
-                weight: row_hash["weight"], height: row_hash["height"], group_id: row_hash["group_id"],
-                exercise: row_hash["exercise"], grooming: row_hash["grooming"], family_friendly: row_hash["family_friendly"],
-                role_id: row_hash["role_id"], temperament: row_hash["temperament"])
-      breed.send("id=", row_hash["id"])
-    end
-    breed
-  end
-
-  def self.find_by_role id
-    database = Environment.database_connection
-    database.results_as_hash = true
-    results = database.execute("SELECT * FROM breeds where breeds.role_id = #{id}")
-    database.results_as_hash = false
-    breeds = []
-    unless results.empty?
-      row_hash = results
-      i = 0
-      results.each do |breed|
-        # group = Group.find_by_id(row_hash[i]["group_id"])
-        breed = Breed.new(name: row_hash[i]["name"], size: row_hash[i]["size"], lifespan: row_hash[i]["lifespan"],
-                  weight: row_hash[i]["weight"], height: row_hash[i]["height"], group_id: row_hash[i]["group_id"],
-                  exercise: row_hash[i]["exercise"], grooming: row_hash[i]["grooming"], family_friendly: row_hash[i]["family_friendly"],
-                  role_id: row_hash[i]["role_id"], temperament: row_hash[i]["temperament"])
-        breed.send("id=", row_hash[i]["id"])
-        breeds << breed
-        i = i +1
-      end
-    end
-    breeds
-  end
-
   def self.return_exercise id
-    database = Environment.database_connection
-    database.results_as_hash = true
-    results = database.execute("select distinct exercise from breeds where role_id = #{id}")
-    database.results_as_hash = false
-    row_hash = results
-    exercises = []
-    i = 0
-    if results.empty?
-      puts "No results found"
-    else
-      results.each do
-        exercise = row_hash[i]["exercise"]
-        exercises << exercise
-        i = i +1
-      end
-    end
-    exercises
+    exercises = Breed.select(:exercise).distinct.where(role_id: id)
+    exercises = exercises.map{ |breed| breed.exercise}
   end
 
   def self.find_top_five(role_id, exercise, grooming, family_friendly)
-    database = Environment.database_connection
-    database.results_as_hash = true
     if family_friendly == "no" && grooming == "yes"
-      results = database.execute("select * from breeds where role_id = #{role_id} and exercise = '#{exercise}' limit 10")
+      results = Breed.where("role_id = #{role_id} and exercise = '#{exercise}'").limit(10)
     elsif family_friendly == "no"
-      results = database.execute("select * from breeds where role_id = #{role_id} and exercise = '#{exercise}' and grooming = '#{grooming}' limit 10")
+      results = Breed.where("role_id = #{role_id} and exercise = '#{exercise}' and grooming = '#{grooming}'").limit(10)
     elsif grooming == "yes"
-      results = database.execute("select * from breeds where role_id = #{role_id} and exercise = '#{exercise}' and family_friendly = '#{family_friendly}' limit 10")
+      results = Breed.where("role_id = #{role_id} and exercise = '#{exercise}' and family_friendly = '#{family_friendly}'").limit(10)
     else
-      results = database.execute("select * from breeds where role_id = #{role_id} and exercise = '#{exercise}' and grooming = '#{grooming}' and family_friendly = '#{family_friendly}' limit 10")
-    end
-    database.results_as_hash = false
-    row_hash = results
-    breeds = []
-    i = 0
-    if results.empty?
-      puts "No results found"
-    else
-      results.each do
-        # group = Group.find_by_id(row_hash[i]["group_id"])
-        breed = Breed.new(name: row_hash[i]["name"], size: row_hash[i]["size"], lifespan: row_hash[i]["lifespan"],
-                  weight: row_hash[i]["weight"], height: row_hash[i]["height"], group_id: row_hash[i]["group_id"],
-                  exercise: row_hash[i]["exercise"], grooming: row_hash[i]["grooming"], family_friendly: row_hash[i]["family_friendly"],
-                  role_id: row_hash[i]["role_id"], temperament: row_hash[i]["temperament"])
-        breed.send("id=", row_hash[i]["id"])
-        breeds << breed
-        i = i +1
-      end
-    end
-    breeds
+      results = Breed.where("role_id = #{role_id} and exercise = '#{exercise}' and grooming = '#{grooming}' and family_friendly = '#{family_friendly}'").limit(10)
+          end
+    results
   end
 
-  protected
-
-  def id=(id)
-    @id = id
-  end
 end
--- test/test_breed.rb
+++ test/test_breed.rb
@@ -3,23 +3,23 @@ require_relative '../models/breed'
 
 class TestBreed < FetchTest
   def test_find_by_name_method_returns_breed_name
-    mountain_dog = Breed.find_by_name("Bernese Mountain Dog")
+    mountain_dog = Breed.find_by(name: "Bernese Mountain Dog")
     assert_equal "Bernese Mountain Dog", mountain_dog.name
   end
 
   def test_find_by_name_method_returns_breed_size
-    mountain_dog = Breed.find_by_name("Australian Kelpie")
+    mountain_dog = Breed.find_by(name: "Australian Kelpie")
     assert_equal "M", mountain_dog.size
   end
 
   def test_to_s_prints_breed_details
-    akita = Breed.find_by_name("Akita")
+    akita = Breed.find_by(name: "Akita")
     expected = "#{akita.id}. Akita:\nSize: L\nLifespan: 10 to 12 years\nAverage Weight: 75 to 110 lbs.\nAverge Height: 24 to 28 inches\nGroup: Working\nExercise: Moderate\nGrooming? yes\nFamily Friendly? yes\nTemperament: The Akita is a bold, independent and stubborn breed."
     assert_equal expected, akita.to_s
   end
 
   def test_search_breeds_by_role
-    breeds = Breed.find_by_role(2)
+    breeds = Breed.where(role_id: 2)
     expected = ["Airedale Terrier", "American Bulldog", "American Staffordshire Terrier", "Anatolian Shepherd", "Belgian Shepherd Malinois", "Blue Lacy", "Bullmastiff", "Cane Corso Mastiff"]
     assert_equal expected, breeds.map{ |breed| breed.name}
   end
