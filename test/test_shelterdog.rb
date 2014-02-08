d8a040e95c7a0a82d20fc35e61969257ee79885e
amanda-thrive <amanda@thrivemg.com>
2014-02-08 12:35:29 -0600

    refactor shelterdog class to be activerecord

--- test/test_shelterdog.rb
+++ test/test_shelterdog.rb
@@ -3,7 +3,7 @@ require_relative '../models/shelterdog'
 
 class TestShelterDog < FetchTest
   def test_to_s_prints_details
-    fido = ShelterDog.new(name: "Fido", breed: "Collie",
+    fido = Shelterdog.create(name: "Fido", breed: "Collie",
                           shelter: "Humane Society", age: "young",
                           weight: "M", status: "A")
     expected = "#{fido.id}. Fido:\n  breed: Collie\n  shelter: Humane Society\n  age: young\n  weight: M\n  status: A"
@@ -11,91 +11,88 @@ class TestShelterDog < FetchTest
   end
 
   def test_saved_shelter_dogs_are_saved
-    fido = ShelterDog.new(name: "Fido", breed: "Collie",
+    fido_before = Shelterdog.count
+    fido = Shelterdog.create(name: "Fido", breed: "Collie",
                           shelter: "Humane Society", age: "young",
                           weight: "M", status: "A")
-    fido_before = ShelterDog.count
-    fido.save
-    fido_after = ShelterDog.count
+    fido_after = Shelterdog.count
     assert_equal fido_before + 1, fido_after
   end
 
   def test_all_returns_empty_array_if_no_shelter_dogs
-    results = ShelterDog.all
+    results = Shelterdog.all
     assert_equal [], results
   end
 
   def test_update_is_reflected_in_existing_instance
-    fido = ShelterDog.create(name: "Fido", breed: "Collie",
+    fido = Shelterdog.create(name: "Fido", breed: "Collie",
                              shelter: "Humane Society", age: "young",
                              weight: "M", status: "A")
-    fido = fido.update("name", "Rover", "#{fido.id}")
+    fido.update(name: "Rover")
     expected = ["Rover", "Collie", "Humane Society", "young", "M", "A" ]
-    actual = [ fido.name, fido.breed, fido.shelter, fido.age, fido.weight, fido.status]
+    actual = [fido.name, fido.breed, fido.shelter, fido.age, fido.weight, fido.status]
     assert_equal expected, actual
   end
 
   def test_update_doesnt_insert_new_row
-    fido = ShelterDog.create(name: "Fido", breed: "Collie",
+    fido = Shelterdog.create(name: "Fido", breed: "Collie",
                              shelter: "Humane Society", age: "young",
                              weight: "M", status: "A")
-    dogs_before = ShelterDog.count
-    fido.update("name", "Rover", "#{fido.id}")
-    dogs_after = ShelterDog.count
+    dogs_before = Shelterdog.count
+    fido.update(name: "Rover")
+    dogs_after = Shelterdog.count
     assert_equal dogs_before, dogs_after
   end
 
   def test_delete_method_removes_dog
-    fido = ShelterDog.create(name: "Fido", breed: "Collie",
+    fido = Shelterdog.create(name: "Fido", breed: "Collie",
                              shelter: "Humane Society", age: "young",
                              weight: "M", status: "A")
-    spot = ShelterDog.create(name: "Spot", breed: "Cocker Spaniel",
+    spot = Shelterdog.create(name: "Spot", breed: "Cocker Spaniel",
                              shelter: "Humane Society", age: "young",
                              weight: "M", status: "A")
-    dogs_before = ShelterDog.count
-    ShelterDog.delete(fido.id)
-    dogs_after = ShelterDog.count
+    dogs_before = Shelterdog.count
+    fido = Shelterdog.find_by(id: fido.id)
+    fido.destroy
+    dogs_after = Shelterdog.count
     assert_equal dogs_before - 1, dogs_after
   end
 
   def test_cant_find_dog_after_it_is_deleted
-    fido = ShelterDog.create(name: "Fido", breed: "Collie",
+    fido = Shelterdog.create(name: "Fido", breed: "Collie",
                              shelter: "Humane Society", age: "young",
                              weight: "M", status: "A")
-    ShelterDog.delete(fido.id)
-    result = ShelterDog.search_by_name(fido.name)
-    assert_equal "The dog you were searching for is not found", result
-  end
-
-  def test_search_by_id_returns_nil_if_unfindable
-    assert_nil ShelterDog.search_by_id(12342)
+    fido = Shelterdog.find_by(id: fido.id)
+    fido.destroy
+    result = Shelterdog.find_by(name: fido.name)
+    assert_nil result
   end
 
   def test_search_shelterdogs_by_breed_name
-    spot = ShelterDog.create(name: "Spot", breed: "Australian Terrier",
+    spot = Shelterdog.create(name: "Spot", breed: "Australian Terrier",
                              shelter: "Humane Society", age: "young",
                              weight: "M", status: "A")
-    fido = ShelterDog.create(name: "Fido", breed: "Australian Terrier",
+    fido = Shelterdog.create(name: "Fido", breed: "Australian Terrier",
                              shelter: "Humane Society", age: "young",
                              weight: "M", status: "A")
-    ruby = ShelterDog.create(name: "Ruby", breed: "German Shepherd",
+    ruby = Shelterdog.create(name: "Ruby", breed: "German Shepherd",
                              shelter: "Humane Society", age: "young",
                              weight: "M", status: "A")
-    shelterdogs = ShelterDog.find_by_breedname("Australian Terrier")
+    shelterdogs = Shelterdog.where(breed: "Australian Terrier")
     assert_equal ["Fido", "Spot"], shelterdogs.map{ |dog| dog.name}
   end
 
   def test_search_shelterdogs_by_breed_name_returns_empty_array
-    spot = ShelterDog.create(name: "Spot", breed: "Australian Terrier",
+    spot = Shelterdog.create(name: "Spot", breed: "Australian Terrier",
                              shelter: "Humane Society", age: "young",
                              weight: "M", status: "A")
-    fido = ShelterDog.create(name: "Fido", breed: "Australian Terrier",
+    fido = Shelterdog.create(name: "Fido", breed: "Australian Terrier",
                              shelter: "Humane Society", age: "young",
                              weight: "M", status: "A")
-    ruby = ShelterDog.create(name: "Ruby", breed: "German Shepherd",
+    ruby = Shelterdog.create(name: "Ruby", breed: "German Shepherd",
                              shelter: "Humane Society", age: "young",
                              weight: "M", status: "A")
-    shelterdogs = ShelterDog.find_by_breedname("Labrador Retriever")
+    shelterdogs = Shelterdog.where(breed: "Labrador Retriever")
     assert_equal [], shelterdogs
   end
 end
